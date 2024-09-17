73b38c05abb66398165b4fd54ab08d869d5bc88d
barbapapazes <e.soubiran25@gmail.com>
2024-09-17 15:43:30 +0200

    fix: update config to handle past sponsors

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'sponsorkit'
+import { defineConfig, tierPresets } from "sponsorkit";
 
 export default defineConfig({
   github: {
-    login: 'barbapapazes',
-    type: 'user',
+    login: "barbapapazes",
+    type: "user",
   },
-  outputDir: '.'
-})
+  tiers: [
+    {
+      title: "Past Sponsors",
+      monthlyDollars: -1,
+      preset: tierPresets.xs,
+    },
+    {
+      title: "Backers",
+      preset: tierPresets.small,
+    },
+  ],
+  outputDir: ".",
+});
